d0f838c4f04158beec29eb9eec288f10006b98a7
BenAshh <55060373+BenAshh@users.noreply.github.com>
2021-10-30 08:18:01 -0700

    updated path msft

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.write(""" # Dashboard Ben  """)
 
-msft = pd.read_csv(r'C:\Users\Ben\Desktop\plannif\Maven\StreamLit\MSFT.csv',index_col='Date')
+msft = pd.read_csv(r'MSFT.csv',index_col='Date')
 msft.index = pd.to_datetime(msft.index)
 msft.rename(columns={'Adj Close':'MSFT'}, inplace=True)
 
